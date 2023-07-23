ad77cb37cc0b81ad05b23ee77ace4e13448d4a87
Ming <98mingshi@gmail.com>
2023-07-23 14:52:23 -0400

    2023-07-23 work

---
+++ 2023_07_23/1.py
@@ -0,0 +1,20 @@
+'''
+Exercise 1: Calculate the multiplication and sum of two numbers
+Given two integer numbers return their product only if the product is equal to or lower than 1000, else return their sum.
+
+Given 1:
+
+number1 = 20
+number2 = 30
+Expected Output: The result is 600
+
+Given 2:
+
+number1 = 40
+number2 = 30
+Expected Output:The result is 70
+'''
+
+
+
+
