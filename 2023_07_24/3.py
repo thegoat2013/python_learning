808010888a2e8287fd42f88956004030b52f0fd4
Ming <98mingshi@gmail.com>
2023-07-23 22:37:16 -0400

    add homework

---
+++ 2023_07_24/3.py
@@ -0,0 +1,16 @@
+'''
+Exercise 7: Return the count of a given substring from a string
+Write a program to find how many times substring “Emma” appears in the given string.
+
+Given:
+
+str_x = "Emma is good developer. Emma is a writer"
+Expected Output:
+
+Emma appeared 2 times
+'''
+
+def countSubString(whole_string, sub_string)
+    #write code here
+
+countSubString("Emma is good developer. Emma is a writer", "Emma")
