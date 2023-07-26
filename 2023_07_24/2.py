f4625f47c374e234ef5206b87885cc83396ec179
ethan <ethan.sy.shi@gmail.com>
2023-07-26 10:17:18 -0400

    done

--- 2023_07_24/2.py
+++ 2023_07_24/2.py
@@ -9,6 +9,11 @@ Exercise 8: Print the following pattern
 def patterns (rows):
     count=1
     times=1
+    numb=1
     for num in range(0,rows):
-        
+        for num in range(0,count):
+            print(str(numb) * count)
+            count=count+1
+            numb=numb+1
+            
 patterns(5)                
