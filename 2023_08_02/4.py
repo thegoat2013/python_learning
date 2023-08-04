3d1730d2f46c332eeec1e4cbc7559eb3dafd55cb
Ming <98mingshi@gmail.com>
2023-08-04 09:53:17 -0400

    add homework

---
+++ 2023_08_02/4.py
@@ -0,0 +1,14 @@
+'''
+Exercise 18: Print the following pattern
+Write a program to print the following start pattern using the for loop
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
+'''
---
+++ 2023_08_02/5.py
@@ -0,0 +1,11 @@
+'''
+Exercise 17: Find the sum of the series upto n terms
+Write a program to calculate the sum of series up to n term. For example, if n =5 the series will become 2 + 22 + 222 + 2222 + 22222 = 24690
+
+Given:
+
+# number of terms
+n = 5
+Expected output:
+24690
+'''
