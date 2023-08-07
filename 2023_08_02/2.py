d554bb815cb7c2bd3ca3a6e5e10faf1654c9ca9c
ethan <ethan.sy.shi@gmail.com>
2023-08-07 09:56:57 -0400

    done

--- 2023_08_02/2.py
+++ 2023_08_02/2.py
@@ -8,3 +8,13 @@ Write a program to use for loop to print the following reverse number pattern
 2 1 
 1
 '''
+def pattern (start):
+    for num in range(start, 0,-1):
+        result=''        
+        for numb in range(num, 0,-1):
+            result=result+' ' + str(numb)
+        print(result)
+pattern(5)        
+           
+    
+    
