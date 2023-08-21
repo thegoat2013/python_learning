0a6bcee6f96025e265a4ec1a115aa46b7919815f
Ming <98mingshi@gmail.com>
2023-08-21 11:01:26 -0400

    add homwork

---
+++ 2023_08_21/5.py
@@ -0,0 +1,10 @@
+'''
+Write a Python program to check if value 200 exists in the following dictionary.
+
+Given:
+
+sample_dict = {'a': 100, 'b': 200, 'c': 300}
+Expected output:
+
+200 present in a dict
+'''
