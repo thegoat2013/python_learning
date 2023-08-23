4252dbda8a3c718fef39661e5069d26128f14817
Ming <98mingshi@gmail.com>
2023-08-22 23:50:39 -0400

    add work

---
+++ 2023_08_22/1.py
@@ -0,0 +1,9 @@
+'''
+Please print out how many times each word in the following paragraph appears.  then print out the word that appears most frequently.
+
+Hint: use a function to split a string to words.
+
+Note: punctuation is not word and should not be counted.
+'''
+
+paragraph = "Ethan is a ten years old boy. He is very smart. He loves to read and plays minecraft.   His favoriate actitivy is to assemble robots.  He has a naughty sister but he really cares about her.  He is a good swimmer and will go to tryout for swim team this weekend.  The only thing he does not like is that he always loses to his daddy during the night fight.  He vows to get better at fighting."
