65fdc809af6eeb95081f41e7c06539feda223186
ethan <ethan.sy.shi@gmail.com>
2023-08-23 10:42:30 -0400

    done

--- 2023_08_22/1.py
+++ 2023_08_22/1.py
@@ -6,4 +6,11 @@ Hint: use a function to split a string to words.
 Note: punctuation is not word and should not be counted.
 '''
 
-paragraph = "Ethan is a ten years old boy. He is very smart. He loves to read and plays minecraft.   His favoriate actitivy is to assemble robots.  He has a naughty sister but he really cares about her.  He is a good swimmer and will go to tryout for swim team this weekend.  The only thing he does not like is that he always loses to his daddy during the night fight.  He vows to get better at fighting."
+paragraph = "Ethan is a ten years old boy. He is very smart. He loves to read and plays minecraft.   His favoriate actitivy is to assemble robots.  He has a naughty sister but he really cares about her.  He is a good swimmer and will go to tryout for swim team this weekend.  One of the things he likes is that he always beats  his daddy during the night fight."
+paragraph = paragraph.split()
+dict={}
+for word in paragraph:
+    for key in dict.keys():
+        if word == key:
+            dict[word] = dict[word]+1
+print(dict)            
