d12ffcf713a5682bd34ab516b1affec752519898
ethan <ethan.sy.shi@gmail.com>
2023-08-31 10:53:12 -0400

    commit

--- 2023_08_22/1.py
+++ 2023_08_22/1.py
@@ -7,10 +7,20 @@ Note: punctuation is not word and should not be counted.
 '''
 
 paragraph = "Ethan is a ten years old boy. He is very smart. He loves to read and plays minecraft.   His favoriate actitivy is to assemble robots.  He has a naughty sister but he really cares about her.  He is a good swimmer and will go to tryout for swim team this weekend.  One of the things he likes is that he always beats  his daddy during the night fight."
-paragraph = paragraph.split()
+words = paragraph.split()
 dict={}
-for word in paragraph:
-    for key in dict.keys():
-        if word == key:
-            dict[word] = dict[word]+1
-print(dict)            
+for word in words:
+    if word in dict:
+        dict[word] = dict[word]+1
+    else:
+        dict[word] = 1
+print(dict)
+max_count=0
+max_word=""
+for word,count in dict.items():
+   if count > max_count:
+       max_count=count
+       max_word=word
+print(max_word)
+print(max_count)
+            
